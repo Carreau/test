84a83f7252e6053c5abb7718b5c9e58a14235509
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2014-02-19 14:57:00 +0100

    simplify conf

--- conf.py
+++ conf.py
@@ -1,15 +1,11 @@
 extensions = ['sphinxcontrib.bibtex']
 
 
-author_first = u'Matthias'
-author_last= u'Bussonnier'
-author_full = u' '.join([author_first,author_last])
-
+author_full = u'Me'
 source_suffix = '.rst'
 master_doc = 'index-latex'
 
 exclude_patterns = ['_build']
-# The name of the Pygments (syntax highlighting) style to use.
 
 latex_documents = [
   ('index-latex', 'actingeldynamics.tex', u'Actin Gels dynamics',
